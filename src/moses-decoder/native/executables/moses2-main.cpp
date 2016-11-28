a97984e94c58c48b7c69be11906b1c3881eb772d
David Madl <git@abanbytes.eu>
2016-11-28 17:28:08 +0100

    moses2 executables

---
+++ src/moses-decoder/native/executables/moses2-main.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <memory>
+#include <boost/pool/pool_alloc.hpp>
+#include "decoder/moses2-main.h"
+#include "../decoder/System.h"
+#include "../decoder/Phrase.h"
+#include "../decoder/TranslationTask.h"
+#include "../decoder/MemPool.h"
+#ifdef HAVE_XMLRPC_C
+#include "server/Server.h"
+#endif
+#include "../decoder/legacy/InputFileStream.h"
+#include "../decoder/legacy/Parameter.h"
+#include "../decoder/legacy/ThreadPool.h"
+#include "../decoder/legacy/Timer.h"
+#include "../decoder/legacy/Util2.h"
+#include "../decoder/legacy/util/usage.hh"
+
+#include "mmt/sentence.h"
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#define ParseWord(w) (boost::lexical_cast<mmt::wid_t>((w)))
+
+using namespace std;
+
+//extern size_t g_numHypos;
+
+static std::vector<mmt::wid_t> ParseSentence(std::string text) {
+  std::vector<std::string> words;
+  std::vector<mmt::wid_t> wids;
+  boost::split(words, text, boost::is_any_of(" "), boost::token_compress_on); // token_compress_on: Compress adjacent tokens
+  wids.resize(words.size());
+  std::transform(words.begin(), words.end(), wids.begin(), [](std::string w) { return ParseWord(w); });
+  return wids;
+}
+
+int main(int argc, const char** argv)
+{
+	cerr << "Starting..." << endl;
+
+  Moses2::Timer timer;
+  timer.start();
+	//Temp();
+
+  Moses2::Parameter params;
+  if (!params.LoadParam(argc, argv)) {
+	return EXIT_FAILURE;
+  }
+  Moses2::System system(params);
+  timer.check("Loaded");
+
+  if (params.GetParam("show-weights")) {
+    return EXIT_SUCCESS;
+  }
+
+  //cerr << "system.numThreads=" << system.options.server.numThreads << endl;
+
+  Moses2::ThreadPool pool(system.options.server.numThreads, system.cpuAffinityOffset, system.cpuAffinityOffsetIncr);
+  //cerr << "CREATED POOL" << endl;
+
+  if (params.GetParam("server")) {
+    std::cerr << "RUN SERVER" << std::endl;
+    run_as_server(system);
+  }
+  else {
+    std::cerr << "RUN BATCH" << std::endl;
+    batch_run(params, system, pool);
+  }
+
+  cerr << "Decoding took " << timer.get_elapsed_time() << " seconds in total, including load time" << endl;
+  //	cerr << "g_numHypos=" << g_numHypos << endl;
+  cerr << "Finished" << endl;
+  return EXIT_SUCCESS;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef HAVE_XMLRPC_C
+void run_as_server(Moses2::System &system)
+{
+  Moses2::Server server(system.options.server, system);
+  server.run(system); // actually: don't return. see Server::run()
+}
+#else
+void run_as_server(Moses2::System &system)
+{
+  throw std::runtime_error("moses was not compiled with xmlrpc, no server available");
+}
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+istream &GetInputStream(Moses2::Parameter &params)
+{
+  const Moses2::PARAM_VEC *vec = params.GetParam("input-file");
+  if (vec && vec->size()) {
+    Moses2::InputFileStream *stream = new Moses2::InputFileStream(vec->at(0));
+    return *stream;
+  }
+  else {
+    return cin;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+void batch_run(Moses2::Parameter &params, Moses2::System &system, Moses2::ThreadPool &pool)
+{
+  istream &inStream = GetInputStream(params);
+
+  long translationId = 0;
+  string line;
+  while (getline(inStream, line)) {
+    //cerr << "line=" << line << endl;
+      boost::shared_ptr<Moses2::TranslationTask> task(new Moses2::TranslationTask(system, ParseSentence(line), translationId));
+
+    //cerr << "START pool.Submit()" << endl;
+    pool.Submit(task);
+    //task->Run();
+    ++translationId;
+  }
+
+  pool.Stop(true);
+
+  if (&inStream != &cin) {
+    delete &inStream;
+  }
+
+  //util::PrintUsage(std::cerr);
+
+}
+////////////////////////////////////////////////////////////////////////////////////////////////
+void Temp()
+{
+	Moses2::MemPool pool;
+	Moses2::MemPoolAllocator<int> a(pool);
+
+	boost::unordered_set<int, boost::hash<int>, std::equal_to<int>, Moses2::MemPoolAllocator<int> > s(a);
+	s.insert(3);
+	s.insert(4);
+	s.insert(3);
+	s.erase(3);
+
+	boost::pool_allocator<int> alloc;
+	std::vector<int, boost::pool_allocator<int> > v(alloc);
+	  for (int i = 0; i < 1000; ++i)
+	    v.push_back(i);
+
+	  v.clear();
+	  boost::singleton_pool<boost::pool_allocator_tag, sizeof(int)>::
+	    purge_memory();
+
+	abort();
+}
---
+++ src/moses-decoder/native/executables/moses2-test.cpp
@@ -0,0 +1,128 @@
+//
+// Created by Davide  Caroselli on 14/01/16.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <thread>
+
+#include <decoder/MosesDecoder.h>
+
+using namespace mmt::decoder;
+
+int run(MosesDecoder *decoder, const std::string *source, std::string *target) {
+  using namespace std;
+
+  translation_t translation;
+  translation = decoder->translate(*source, 0, NULL, 0);
+  *target = translation.text;
+
+  return 0;
+}
+
+void batch_run(MosesDecoder *decoder) {
+  using namespace std;
+  vector<string> lines;
+  string line;
+
+  while(getline(cin, line)) {
+    lines.push_back(line);
+  }
+
+  vector<string> translations;
+  translations.resize(lines.size());
+  thread threads[lines.size()];
+
+  size_t i = 0;
+  size_t nthreads = 8;
+  for(i = 0; i < nthreads && i < lines.size(); i++) {
+    threads[i] = thread(&run, decoder, &lines[i], &translations[i]);
+    //threads[i].join();
+    //cout << translations[i] << endl;
+  }
+
+  size_t j = 0;
+  for(; j < lines.size(); j++) {
+    threads[j].join();
+    cout << translations[j] << endl;
+    if(i < lines.size()) {
+      threads[i] = thread(&run, decoder, &lines[i], &translations[i]);
+      i++;
+    }
+  }
+}
+
+int main(int argc, const char *argv[]) {
+  if (argc < 2) {
+    printf("USAGE: jnitest MOSES_INI\n");
+    return 1;
+  }
+
+  const char *moses_ini = argv[1];
+  std::cout << "Moses INI: " << moses_ini << "\n";
+
+  MosesDecoder *decoder = MosesDecoder::createInstance(moses_ini, nullptr, nullptr);
+
+  // Features
+  std::vector<feature_t> features = decoder->getFeatures();
+  for (int i = 0; i < features.size(); i++) {
+    std::cout << "Feature: " << features[i].name << " ";
+    std::vector<float> weights = decoder->getFeatureWeights(features[i]);
+
+    for (int j = 0; j < weights.size(); j++)
+      std::cout << weights[j] << " ";
+
+    std::cout << "\n";
+  }
+  std::cout << "\n";
+
+  if(argc == 3) {
+    batch_run(decoder);
+    return 0;
+  }
+
+  // Translation
+  //std::string text = "system information support";
+  std::string text = "1092 10121 10010 1049 1006";
+  translation_t translation;
+
+  //Vocabulary::Lookup(const vector<vector<string>> &buffer, vector<vector<wid_t>> *output, bool putIfAbsent)
+  // but we have no Vocabulary.
+
+  std::map<std::string, float> ibm;;
+  ibm["2"] = 1.f;
+  std::map<std::string, float> europarl;
+  europarl["1"] = 1.f;
+
+  translation = decoder->translate(text, 0, NULL, 0);
+  std::cout << "Translation: " << translation.text << "\n";
+  std::cout << "Alignment: ";
+  for(std::vector<std::pair<size_t, size_t> >::iterator it = translation.alignment.begin(); it != translation.alignment.end(); it++)
+    std::cout << it->first << "-" << it->second << " ";
+  std::cout << "\n";
+  translation = decoder->translate(text, 0, &ibm, 0);
+  std::cout << "Translation IBM: " << translation.text << "\n";
+  translation = decoder->translate(text, 0, &europarl, 0);
+  std::cout << "Translation EUR: " << translation.text << "\n";
+  std::cout << "\n";
+
+  int64_t ibmSession = decoder->openSession(ibm);
+  int64_t europarlSession = decoder->openSession(europarl);
+
+  std::cout << "Opened Translation Session IBM ID: " << ibmSession << "\n";
+  std::cout << "Opened Translation Session EUR ID: " << europarlSession << "\n";
+
+  translation = decoder->translate(text, ibmSession, NULL, 0);
+  std::cout << "Translation Session IBM: " << translation.text << "\n";
+  translation = decoder->translate(text, europarlSession, NULL, 0);
+  std::cout << "Translation Session EUR: " << translation.text << "\n";
+  std::cout << "\n";
+
+  translation = decoder->translate(text, 0, NULL, 10);
+  for (int i = 0; i < translation.hypotheses.size(); i++) {
+    hypothesis_t hyp = translation.hypotheses[i];
+    std::cout << "Translation HYP: " << hyp.text << " ||| " << hyp.fvals << " ||| " << hyp.score << "\n";
+  }
+
+  return 0;
+}
